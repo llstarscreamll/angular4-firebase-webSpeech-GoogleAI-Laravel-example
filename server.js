12ef593dcee47a12d6bbfc5cac740592e5fca6d5
Johan Alvarez <llstarscreamll@hotmail.com>
2017-09-09 15:00:40 -0500

    fixing serve

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const path = require('path');
 const compression = require('compression');
 app.use(compression());
-
+/*
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
 const forceSSL = function() {
   return function (req, res, next) {
+    console.log(req.get('Host'));
+    return next();
+
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
        ['https://', req.get('Host'), req.url].join('')
@@ -31,4 +35,19 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default
 // Heroku port
+console.log(process.env.PORT || 8080);
 app.listen(process.env.PORT || 8080);
+
+*/
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.set('post', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
